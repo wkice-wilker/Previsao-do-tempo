824e5777dcf65934ca33f8507a097a2be323c3fe
wkice-wilker <92658448+wkice-wilker@users.noreply.github.com>
2023-09-17 22:40:31 -0300

    fundo

--- app.js
+++ app.js
@@ -295,7 +295,7 @@ if (['01n','02n','03n','04n','09n','10n','11n','50n'].includes(icon)) {
   }else if (['02n'].includes(icon)) {
 
     //nuvem noite
-    fundoEfeito.src = './efeito/noite.png';
+    fundoEfeito.src = './efeito/noitenuvem.png';
     weatherIconefeito.src = './efeito/nuvem.png';
 
   } else if (['09d' , '10d'].includes(icon)) {
